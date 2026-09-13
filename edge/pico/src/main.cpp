acac99511ce207b7a77123b3b7a32baccdea78bc
daigo424 <ishii.daigo@gmail.com>
2026-09-13 16:19:54 +0900

    refactor: organize and integrate code by sensor type

--- edge/pico/src/main.cpp
+++ edge/pico/src/main.cpp
@@ -1,7 +1,7 @@
-//#include "one-motor/main.hpp"
-//#include "two-motor/main.hpp"
-//#include "ros-debug/main.hpp"
-//#include "alternate-motors/main.hpp"
+//#include "motor/one-motor/main.hpp"
+//#include "motor/two-motor/main.hpp"
+//#include "motor/ros-debug/main.hpp"
+//#include "motor/alternate-motors/main.hpp"
 #include "production/main.hpp"
 
 void setup() {
